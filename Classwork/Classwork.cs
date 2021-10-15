87fe69bcae161b4c94405dbc8227fe16f73d3f2c
Рафаэль Халилулин <RFKhalilulin@stud.kpfu.ru>
2021-10-15 22:30:49 +0300

    Added solution for task 4-5 (Task 4 not finished)

--- Classwork/Classwork.cs
+++ Classwork/Classwork.cs
@@ -12,7 +12,34 @@ namespace Classwork
         private const string PathToListStudent = @"..\..\files\ListStudents.txt";
         private const string PathToListEmployees = @"..\..\files\ListEmployees.txt";
         private const string PathToListTablesForEmployees = @"..\..\files\ListTables.txt";
-
+        private const string PathToListHospitals = @"..\..\files\ListHospitals.txt";
+        private const string PathToListMatesEmployees = @"..\..\files\ListMatesEmployees.txt";
+        static List<string> listMates = new List<string>();
+        enum Disease : byte
+        {
+            Оспа = 1,
+            Грипп,
+            Чахотка,
+            Covid19,
+            Артрит,
+            Сифилис,
+            Гепатит,
+            Лихорадка
+        }
+        struct Granny
+        {
+            public string name;
+            public byte age;
+            public List<Disease> diseases;
+            public string medicine;
+        }
+        struct Hospital
+        {
+            public string name;
+            public List<Disease> treatment;
+            public List<Granny> patient;
+            public ushort capacity;
+        }
         struct Student
         {
             public string name;
@@ -41,7 +68,7 @@ namespace Classwork
             //DoTask1();
             //DoTask2();
             //DoTask3();
-            //DoTask4();
+            DoTask4();
             //DoTask5();
             Console.ReadKey();
         }
@@ -121,7 +148,7 @@ namespace Classwork
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.Добавить нового студента\n2.Удалить студента по имени и фамилии\n3. Сортировать по баллам" +
+                Console.WriteLine("\n1.Добавить нового студента\n2.Удалить студента по имени и фамилии\n3. Сортировать по баллам" +
                 "\nДругой: Закончить программу");
                 Console.Write("Выберите действие. Нажми цифру: ");
                 ConsoleKey choise = Console.ReadKey().Key;
@@ -183,22 +210,266 @@ namespace Classwork
         {
             Queue<Employee> queEmployee = new Queue<Employee>();
             Stack<Table> stackTable = new Stack<Table>();
-            Stack<Table> containerStack = new Stack<Table>();
+            Stack<Table> containerTable = new Stack<Table>();
+            
+            using (StreamReader fileTextRead = new StreamReader(PathToListMatesEmployees))
+            {
+                string stringfromfile;
+                while ((stringfromfile = fileTextRead.ReadLine()) != null)
+                {
+                    listMates.Add(stringfromfile);
+                }
+            }
             AddTableFromFile(ref stackTable);
             FillQueueOfEmployees(ref queEmployee);
             while (queEmployee.Count != 0)
             {
                 Employee currentEmployee = queEmployee.Dequeue();
-                //stackTable
+                Table tableLastFree;
+                while (stackTable.Count != 0)
+                {
+                    Table currentTable = stackTable.Pop();
+                    if (currentEmployee.stupidity)
+                    {
+                        if(currentTable.persons.Count < 4)
+                        {
+                            if (currentTable.persons.Count == 0)
+                            {
+                                tableLastFree =  currentTable;
+                            }
+                            foreach (var item in currentTable.persons)
+                            {
+                                if (!GetExistingConnectionEmployees(item, currentEmployee))
+                                {
+                                    currentTable.persons.Add(currentEmployee);
+                                    stackTable.Push(currentTable);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            containerTable.Push(currentTable);
+                        }
+                    }
+                    else
+                    {
+                        if (currentEmployee.impudence > 0)
+                        {
+                            if (currentTable.persons.Count < 5)
+                            {
+                                if (currentTable.persons.Count == 0)
+                                {
+                                    tableLastFree = currentTable;
+                                }
+                                foreach (var item in currentTable.persons)
+                                {
+                                    if (GetExistingConnectionEmployees(item, currentEmployee))
+                                    {
+                                        currentTable.persons.Add(currentEmployee);
+                                        stackTable.Push(currentTable);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                containerTable.Push(currentTable);
+                            }
+                        }
+                        else
+                        {
+                            if (currentTable.persons.Count < 4)
+                            {
+                                if (currentTable.persons.Count == 0)
+                                {
+                                    tableLastFree = currentTable;
+                                }
+                                foreach (var item in currentTable.persons)
+                                {
+                                    if (GetExistingConnectionEmployees(item, currentEmployee))
+                                    {
+                                        currentTable.persons.Add(currentEmployee);
+                                        stackTable.Push(currentTable);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                containerTable.Push(currentTable);
+                            }
+                        }
+                    }
+                    while (containerTable.Count != 0)
+                    {
+                        stackTable.Push(containerTable.Pop());
+                    }
+                }
+                
             }
-
-
-
+            Console.WriteLine("\nСписок столов: ");
+            foreach (var item in stackTable)
+            {
+                Console.WriteLine("Номер стола - " + item.number);
+                Console.WriteLine("Цвет стола - " + item.colour);
+                Console.WriteLine("Персоны за столом: ");
+                foreach (var person in item.persons)
+                {
+                    Console.WriteLine(person.name + " " + person.position);
+                }
+            }
 
         }
         static void DoTask5()
         {
-
+            Queue<Granny> visitorsGranny = new Queue<Granny>();
+            Stack<Granny> grannyDead = new Stack<Granny>();
+            Stack<Hospital> hospitals = new Stack<Hospital>();
+            Stack<Hospital> containerHospitals = new Stack<Hospital>();
+            using (StreamReader fileTextRead = new StreamReader(PathToListHospitals))
+            {
+                string stringfromfile;
+                int numberString = 1;
+                while ((stringfromfile = fileTextRead.ReadLine()) != null)
+                {
+                    string[] dateHospital = stringfromfile.Split('\\');
+                    if (dateHospital.Length != 3)
+                    {
+                        Console.WriteLine($"Длина {numberString} строки не соответсвует формату");
+                    }
+                    else
+                    {
+                        Hospital newHospital;
+                        newHospital.name = dateHospital[0];
+                        newHospital.treatment = new List<Disease>();
+                        for (int i = 0; i < dateHospital[1].Length; i++)
+                        {
+                            //if (byte.TryParse((string)dateHospital[1][i], out _))
+                            //{
+                            //    Не понимаю))
+                            //}
+                            newHospital.treatment.Add((Disease)(byte)dateHospital[1][i]);
+                        }
+                        if (!ushort.TryParse(dateHospital[2], out newHospital.capacity))
+                        {
+                            throw new FormatException($"Ошибка вместимости больницы строка №{numberString} в файле списка больниц");
+                        }
+                        newHospital.patient = new List<Granny>();
+                        hospitals.Push(newHospital);
+                    }
+                    numberString++;
+                }
+            }
+            Console.Write("Введите количество бабуль: ");
+            if (!ushort.TryParse(Console.ReadLine(), out ushort numGranny))
+            {
+                throw new FormatException("Wrong format numGranny (DoTask5)");
+            }
+            for (int i = 1; i < numGranny+1; i++)
+            {
+                Console.WriteLine($"Бабуля №{i}");
+                Granny newGranny;
+                Console.Write("Введите имя: ");
+                newGranny.name = Console.ReadLine();
+                Console.Write("Введите возраст: ");
+                if (!byte.TryParse(Console.ReadLine(), out newGranny.age)|| newGranny.age < 40)
+                {
+                    Console.WriteLine("Возраст имеет неправильный формат. Попробуй снова");
+                    i--;
+                    continue;
+                }
+                newGranny.diseases = new List<Disease>();
+                Console.WriteLine("Список болезней:\n(1)Оспа (2)Грипп (3)Чахотка" +
+                    " (4)Covid19\n(5)Артрит (6)Сифилис (7)Гепатит (8)Лихорадка (Другое)\"Больше\"Нет болезней");
+                bool flagExistingDesies = true;
+                while (flagExistingDesies)
+                {
+                    Console.Write("Введите болезень: ");
+                    if (!byte.TryParse(Console.ReadLine(), out byte choise) || choise == 0 || choise > 8)
+                    {
+                        flagExistingDesies = false;
+                    }
+                    else
+                    {
+                        if (newGranny.diseases.Contains((Disease)choise))
+                        {
+                            Console.WriteLine("У бабушки уже есть такая болезнь!");
+                        }
+                        else
+                        {
+                            newGranny.diseases.Add((Disease)choise);
+                        }
+                    }
+                }
+                Console.Write("Введите лекартсво: ");
+                newGranny.medicine = Console.ReadLine();
+                visitorsGranny.Enqueue(newGranny);
+            }
+            while (visitorsGranny.Count != 0)
+            {
+                Granny grannyCurrent = visitorsGranny.Dequeue();
+                Console.WriteLine("Имя бабули: " + grannyCurrent.name + "\tЛекарство бабули: " +  grannyCurrent.medicine 
+                    + "\tВозраст бабули: " + grannyCurrent.age);
+                Console.WriteLine("Болезни бабушки: ");
+                foreach (var item in grannyCurrent.diseases)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+                bool flagFound = true;
+                while (hospitals.Count != 0  && flagFound)
+                {
+                    //ref Hospital  hospitalCurrent = ref hospitals.Pop(); почему?
+                    Hospital  hospitalCurrent = hospitals.Pop();
+                    if (hospitalCurrent.capacity != 0)
+                    {
+                        if (grannyCurrent.diseases.Count == 0)
+                        {
+                            hospitalCurrent.patient.Add(grannyCurrent);
+                            hospitalCurrent.capacity--;
+                            hospitals.Push(hospitalCurrent);
+                            flagFound = false;
+                        }
+                        else if (KnowFateGranny(grannyCurrent.diseases, hospitalCurrent.treatment))
+                        {
+                            hospitalCurrent.patient.Add(grannyCurrent);
+                            hospitalCurrent.capacity--;
+                            hospitals.Push(hospitalCurrent);
+                            flagFound = false;
+                        }
+                        else
+                        {
+                            containerHospitals.Push(hospitalCurrent);
+                        }
+                    }
+                    else
+                    { 
+                        containerHospitals.Push(hospitalCurrent);
+                    }
+                }
+                while (containerHospitals.Count != 0)
+                {
+                    hospitals.Push(containerHospitals.Pop());
+                }
+                if (flagFound)
+                {
+                    grannyDead.Push(grannyCurrent);
+                }
+            }
+            Console.WriteLine("\n\nСписок Больниц");
+            foreach (var item in hospitals)
+            {
+                Console.WriteLine($"\n\nНазвание больницы: {item.name}");
+                Console.WriteLine($"В больнице лечат: ");
+                foreach (var treatment in item.treatment)
+                {
+                    Console.WriteLine(treatment + "\t");
+                }
+                Console.WriteLine($"В больнице лечатcя: ");
+                foreach (var patient in item.patient)
+                {
+                    Console.WriteLine(patient.name + " " + patient.age + "лет(год)");
+                }
+                Console.WriteLine($"В больнице осталось свободных мест: {item.capacity}");
+            }
         }
         static string GetResultOfGameVikings(byte[] a, byte[] b)
         {
@@ -384,6 +655,31 @@ namespace Classwork
                     numberString++;
                 }
             }
+        }
+        static bool GetExistingConnectionEmployees(Employee employee1, Employee employee2)
+        {
+            foreach (var item in listMates)
+            {
+                string strNameEmp1 = employee1.name + " " + employee1.position;
+                string strNameEmp2 = employee2.name + " " + employee2.position;
+                if (item.Contains(strNameEmp1) && item.Contains(strNameEmp2))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static bool KnowFateGranny(List<Disease> diseasesGranny, List<Disease> treatmentsHospital)
+        {
+            byte NumberCurableDisease = 0;
+            foreach (var item in diseasesGranny)
+            {
+                if (treatmentsHospital.Contains(item))
+                {
+                    NumberCurableDisease++;
+                }
+            }
+            return (double)NumberCurableDisease / diseasesGranny.Count() >= 0.5;
         }
     }
 }
--- Homework/Homework.cs
+++ Homework/Homework.cs
@@ -10,6 +10,7 @@ namespace Homework
         {
             DoTask1();
             DoTask2();
+            
             Console.ReadKey();
         }
         static void DoTask1()
